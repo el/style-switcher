63c09d39779a4c4cc80e37b9ed379fb1b7e00deb
Eliz <elizkilic@gmail.com>
2020-07-08 21:46:00 +0300

    Bump up the default style versions

--- dist/index.js
+++ dist/index.js
@@ -16,14 +16,19 @@ class MapboxStyleSwitcherControl {
         this.controlContainer.classList.add("mapboxgl-ctrl-group");
         this.mapStyleContainer = document.createElement("div");
         this.styleButton = document.createElement("button");
+        this.styleButton.type = "button";
         this.mapStyleContainer.classList.add("mapboxgl-style-list");
         for (const style of this.styles) {
             const styleElement = document.createElement("button");
+            styleElement.type = "button";
             styleElement.innerText = style.title;
             styleElement.classList.add(style.title.replace(/[^a-z0-9-]/gi, '_'));
             styleElement.dataset.uri = JSON.stringify(style.uri);
             styleElement.addEventListener("click", event => {
                 const srcElement = event.srcElement;
+                if (srcElement.classList.contains("active")) {
+                    return;
+                }
                 this.map.setStyle(JSON.parse(srcElement.dataset.uri));
                 this.mapStyleContainer.style.display = "none";
                 this.styleButton.style.display = "block";
@@ -68,11 +73,11 @@ class MapboxStyleSwitcherControl {
 }
 MapboxStyleSwitcherControl.DEFAULT_STYLE = "Streets";
 MapboxStyleSwitcherControl.DEFAULT_STYLES = [
-    { title: "Dark", uri: "mapbox://styles/mapbox/dark-v9" },
-    { title: "Light", uri: "mapbox://styles/mapbox/light-v9" },
-    { title: "Outdoors", uri: "mapbox://styles/mapbox/outdoors-v10" },
-    { title: "Satellite", uri: "mapbox://styles/mapbox/satellite-streets-v10" },
-    { title: "Streets", uri: "mapbox://styles/mapbox/streets-v10" }
+    { title: "Dark", uri: "mapbox://styles/mapbox/dark-v10" },
+    { title: "Light", uri: "mapbox://styles/mapbox/light-v10" },
+    { title: "Outdoors", uri: "mapbox://styles/mapbox/outdoors-v11" },
+    { title: "Satellite", uri: "mapbox://styles/mapbox/satellite-streets-v11" },
+    { title: "Streets", uri: "mapbox://styles/mapbox/streets-v11" }
 ];
 exports.MapboxStyleSwitcherControl = MapboxStyleSwitcherControl;
 //# sourceMappingURL=index.js.map
--- lib/index.ts
+++ lib/index.ts
@@ -10,17 +10,17 @@ export class MapboxStyleSwitcherControl implements IControl
 {
     private static readonly DEFAULT_STYLE = "Streets";
     private static readonly DEFAULT_STYLES = [
-        { title: "Dark", uri:"mapbox://styles/mapbox/dark-v9"},
-        { title: "Light", uri:"mapbox://styles/mapbox/light-v9"},
-        { title: "Outdoors", uri:"mapbox://styles/mapbox/outdoors-v10"},
-        { title: "Satellite", uri:"mapbox://styles/mapbox/satellite-streets-v10"},
-        { title: "Streets", uri:"mapbox://styles/mapbox/streets-v10"}
+        { title: "Dark", uri:"mapbox://styles/mapbox/dark-v10"},
+        { title: "Light", uri:"mapbox://styles/mapbox/light-v10"},
+        { title: "Outdoors", uri:"mapbox://styles/mapbox/outdoors-v11"},
+        { title: "Satellite", uri:"mapbox://styles/mapbox/satellite-streets-v11"},
+        { title: "Streets", uri:"mapbox://styles/mapbox/streets-v11"}
     ];
 
     private controlContainer: HTMLElement | undefined;
     private map?: MapboxMap;
     private mapStyleContainer: HTMLElement | undefined;
-    private styleButton: HTMLElement | undefined;
+    private styleButton: HTMLButtonElement | undefined;
     private styles: MapboxStyleDefinition[];
 
     constructor(styles?: MapboxStyleDefinition[])
@@ -43,16 +43,22 @@ export class MapboxStyleSwitcherControl implements IControl
         this.controlContainer.classList.add("mapboxgl-ctrl-group");
         this.mapStyleContainer = document.createElement("div");
         this.styleButton = document.createElement("button");
+        this.styleButton.type = "button";
         this.mapStyleContainer.classList.add("mapboxgl-style-list");
         for (const style of this.styles)
         {
             const styleElement = document.createElement("button");
+            styleElement.type = "button";
             styleElement.innerText = style.title;
             styleElement.classList.add(style.title.replace(/[^a-z0-9-]/gi, '_'));
             styleElement.dataset.uri = JSON.stringify(style.uri);
             styleElement.addEventListener("click", event =>
             {
                 const srcElement = event.srcElement as HTMLButtonElement;
+                if (srcElement.classList.contains("active"))
+                {
+                    return;
+                }
                 this.map!.setStyle(JSON.parse(srcElement.dataset.uri!));
                 this.mapStyleContainer!.style.display = "none";
                 this.styleButton!.style.display = "block";
